5c62b678ec03a3fedd69faba137aecd2761de0a3
Marcos <marcosmperes@gmail.com>
2021-06-17 16:50:48 -0300

    Update ControlaCenaCoppelia.lua
    Atualização para a última versão.

--- ControlaCenaCoppelia.lua
+++ ControlaCenaCoppelia.lua
@@ -1,37 +1,39 @@
--- This code is provided by Eric Rohmer
--- erohmer@gmail.com
--- you can use it without any warranty and modify reuse and distribute it at will
-
 sim.setThreadSwitchTiming(2) -- Default timing for automatic thread switching
 simDelegateChildScriptExecution()
+
+-- Put some initialization code here
+
+-- definindo os parametros da serial
+portNumber="\\\\.\\COM3" -- definindo a porta serial
+baudrate=9600 -- definindo a velocidade de comunicacao pela porta (deve ser igual a do Arduino)
+serial=sim.serialOpen(portNumber,baudrate) -- habilitando a porta serial
+
+--Definindo os manipuladores (handle)
+baseCarroRobo=sim.getObjectAssociatedWithScript(sim.handle_self) -- this is CarroRobo's handle
+leftMotor=sim.getObjectHandle("CarroRobo_motorEsq") -- Handle of the left motor
+rightMotor=sim.getObjectHandle("CarroRobo_motorDir") -- Handle of the right motor
+leftSensor=sim.getObjectHandle("CarroRobo_sensorEsq") -- Handle of the left sensor
+rightSensor=sim.getObjectHandle("CarroRobo_sensorDir") -- Handle of the right sensor
+obstaculo = sim.getObjectHandle("Obstacle")
+layer = sim.setObjectInt32Parameter(obstaculo,10,256)
 
 -- Inicializacao de variaveis
 novoModo = "m"
 novaVeloc = 120
 novoTempo = 100
-tecla = "w"
+tecla = "q"
 estadoParado=0
-direcao = "w"
+direcao = "q"
+contaPassos =0
 
---defining the serial parameters 
-portNumber="\\\\.\\COM3" -- definindo a porta serial
-baudrate=9600 -- definindo a velocidade de comunicacao pela porta (deve ser igual a do Arduino)
-serial=sim.serialOpen(portNumber,baudrate) -- habilitando a porta serial
+-- Conversao de valor grau para radianos
+minMaxVeloc={50*math.pi/180,135*math.pi/180} -- Min and max speeds for each motor (Em radianos/seg)
+--print(minMaxVeloc[1],minMaxVeloc[2])
 
---getting the cube handle and initial pose
-
---ref = sim.getObjectHandle('Sphere')
-h=sim.getObjectHandle('Cuboid')
-refAbs = h
-cam=sim.getObjectHandle('DefaultCamera')
-ipos=sim.getObjectPosition(h,-1)
-iori=sim.getObjectOrientation(h,-1)
-
-xml = [[
-<ui title="Controles Carro" closeable="false" resizable="false" activate="false">
-    <group layout="form" flat="true">
-        <label text="Velocidade: 120" id="1"/>
-        <hslider tick-position="above" tick-interval="31" minimum="100" maximum="255" on-change="alteraVeloc" id="2"/>
+-- Create the custom UI:
+    xml = '<ui title="Controle '..sim.getObjectName(baseCarroRobo)..'" closeable="false" resizeable="false" activate="false">'..[[
+        <label text="Velocidade motores: 92.5 deg/s" id="1"/>
+        <hslider minimum="0" maximum="100" on-change="velocChange_callback" id="2"/>
         <label text="Tempo (ms): 100" id="3"/>
         <hslider tick-position="above" tick-interval="100" minimum="100" maximum="700" on-change="alteraTempo" id="4"/>
         <label text="Modo" id="5"/>
@@ -40,34 +42,30 @@ xml = [[
         <button text="PARAR" on-click="paradaEmergencia" checked="false" checkable="true" id="8"/>
         <label text="Sincronizar dados" id="9"/>
         <button text="SINCRONIZAR" on-click="enviaStringSerial" auto-repeat="true" auto-repeat-interval="1000" checkable="true" id="10"/>
-    </group>
     <label text="" style="* {margin-left: 400px;}"/>
-</ui>
-]]
+    </ui>
+    ]]
 uiControle=simUI.create(xml)
 
 xmlSensors = [[
 <ui title="Acompanhamento Sensores" closeable="false" resizable="false" activate="false">
-    <group layout="grid" flat="true">
         <label text="Esquerdo: 00.0 mm e Direito: 00.0 mm" id="1"/>
-    </group>
+        <label text="" id="2"/>
     <label text="" style="* {margin-left: 300px;}"/>
 </ui>
 ]]
 uiSensors=simUI.create(xmlSensors)
 
---xmlAlerta = [[
---<ui title="Alerta de proximidade!" closeable="true" on-close="" resizable="false" activate="false">
-    
---   <label text="Obstaculo Proximo!" id="1"/>
---    <label text="" style="* {margin-left: 300px;}"/>
---</ui>
---]]
+veloc=(minMaxVeloc[1]+minMaxVeloc[2])*0.5
+--print(veloc)
+simUI.setSliderValue(uiControle,2,100*(veloc-minMaxVeloc[1])/(minMaxVeloc[2]-minMaxVeloc[1]))
 
-
-function alteraVeloc(uiControle,id,newVal)
-    novaVeloc = newVal
-    simUI.setLabelText(uiControle,1,string.format("Velocidade: %i",novaVeloc))
+function velocChange_callback(uiControle,id,newVal)
+    veloc = minMaxVeloc[1]+((minMaxVeloc[2]-minMaxVeloc[1])*newVal/100)
+    simUI.setLabelText(uiControle,1,string.format("Velocidade motores: %.1f deg/s",(veloc*180/math.pi)))
+    novaVeloc = (veloc/math.pi)*255*1.3334 -- compensando a diferen?a de velociadade do carro virtual para o fisico
+    andaDirecao()
+    --print(novaVeloc)
 end
 
 function alteraTempo(uiControle,id,newVal)
@@ -79,13 +77,16 @@ function alteraModo(uiControle,id,modoCB)
     -- estados 0 ou 2, desmarcado ou marcado respectivamente
     if modoCB==0 then
         novoModo = "m" -- Modo manual
+        tecla = "q"
+        direcao = tecla
         print(novoModo)
-        enviaStringSerial()
     else
         novoModo = "k" -- Modo automatico
+        tecla = "w"
         print(novoModo)
-        enviaStringSerial()
     end
+    andaDirecao()
+    enviaStringSerial()
 end
 
 function paradaEmergencia(uiControle,id,newVal)
@@ -105,23 +106,23 @@ function paradaEmergencia(uiControle,id,newVal)
     end
 end
 
-function andaDirecao(direcao) --religar simulacao
-    if direcao == "w" then
-        ipos[1] = ipos[1]+0.2;
-        print("Ori Frente X",iori[1],", Y ",iori[2],", Z ",iori[3])
-        sim.setObjectPosition(h,refAbs,{ipos[1],ipos[2],ipos[3]})
-    elseif direcao == "s" then
-        ipos[1] = ipos[1]-0.2;
-        print("Ori Tras X",iori[1],", Y ",iori[2],", Z ",iori[3])
-        sim.setObjectPosition(h,refAbs,{ipos[1],ipos[2],ipos[3]})
-    elseif direcao == "a" then
-        iori[3] = iori[3]+1.4*(math.pi/180);
-        print("Ori Esquerda X",iori[1],", Y ",iori[2],", Z ",iori[3])
-        sim.setObjectOrientation(h,refAbs,{iori[1],iori[2],iori[3]})
-    elseif direcao == "d" then
-        iori[3] = iori[3]-1.4*(math.pi/180);
-        print("Ori Direita X",iori[1],", Y ",iori[2],", Z ",iori[3])
-        sim.setObjectOrientation(h,refAbs,{iori[1],iori[2],iori[3]})
+
+function andaDirecao() --religar simulacao
+    if direcao == "w" or tecla == "w" then -- frente
+        sim.setJointTargetVelocity(leftMotor,veloc) -- ao passar a velocidade para o motor, ela sera convertida para grau/seg
+        sim.setJointTargetVelocity(rightMotor,veloc)
+    elseif direcao == "s" or tecla == "s" then -- tras
+        sim.setJointTargetVelocity(leftMotor,-veloc) -- ao passar a velocidade para o motor, ela sera convertida para grau/seg
+        sim.setJointTargetVelocity(rightMotor,-veloc)
+    elseif direcao == "a" or tecla == "a" then -- esquerda
+        sim.setJointTargetVelocity(leftMotor,-veloc) -- ao passar a velocidade para o motor, ela sera convertida para grau/seg
+        sim.setJointTargetVelocity(rightMotor,veloc)
+    elseif direcao == "d" or tecla == "d" then -- direita
+        sim.setJointTargetVelocity(leftMotor,veloc) -- ao passar a velocidade para o motor, ela sera convertida para grau/seg
+        sim.setJointTargetVelocity(rightMotor,-veloc)
+    elseif direcao == "q" or tecla == "q" then -- parar
+        sim.setJointTargetVelocity(leftMotor,0) -- ao passar a velocidade para o motor, ela sera convertida para grau/seg
+        sim.setJointTargetVelocity(rightMotor,0)
     end
 end
 
@@ -140,86 +141,100 @@ function imprimeVal()
     end
 end
 
-while (sim.getSimulationState()~=sim.simulation_advancing_abouttostop) do
 
+-- Put your main loop here, e.g.:
+--
+while sim.getSimulationState()~=sim.simulation_advancing_abouttostop do
     -- Aqui le-se uma linha a partir da Serial
-    str=sim.serialRead(serial,100,true,'\r\n',100) -- armazena todo o texto em Str ate encontrar "\r\n"
-    --print(str)
-    if str ~= nil then -- Verifica se o valor recebido nao e vazio (nulo = nil)
+    txtSerial=sim.serialRead(serial,100,true,'\r\n',100) -- armazena todo o texto em Str ate encontrar "\r\n"
+
+    if txtSerial ~= nil then -- Verifica se o valor recebido nao e vazio (nulo = nil)
         local token -- cria uma variavel apenas nesse contexto chamada token 
         val={} -- cria um vetor (array) para armazenar as informacoes que chegarem
         cpt=0 -- criou um contador para modificar as posicoes no vetor
-        -- extraindo os valores na string Str separados por virgula
-        for token in string.gmatch(str, "[^,]+") do -- para cada valor fora a virgula, sera salvo em token
+        -- extraindo os valores na txtSerialing txtSerial separados por virgula
+        for token in string.gmatch(txtSerial, "[^,]+") do -- para cada valor fora a virgula, sera salvo em token
             cpt=cpt+1 -- adiciona 1 para armazenar no proximo espaco do vetor
             val[cpt]= token -- armazena cada token em um espaco do vetor
         end
-        val[1] = tonumber(val[1])
-        val[2] = tonumber(val[2])
+        val[1] = tonumber(val[1]) --Leitura de ditancia do sensor ultrassonico da esquerda
+        val[2] = tonumber(val[2]) --Leitura de ditancia do sensor ultrassonico da direita
     end
     
     --imprimeVal()
-    if val[3] ~= nil then
+    if val[3] ~= nil and novoModo == "k" then
         direcao = val[3]
-        andaDirecao(direcao)
+        tecla = direcao
+        andaDirecao()
     end
-    
-    -- Aqui mostra os valores dos sensores
+    -- Valores dos sensores fisicos
     if (val[1]~= 0 and val[2]~= 0) then
         simUI.setLabelText(uiSensors,1,string.format("Sensor1: %.1f mm e Sensor2: %.1f mm",val[1], val[2]))
+        if ((val[1] + val[2]) <= 260) or val[1]<=160 or val[2]<=160 then
+            simUI.setLabelText(uiSensors,2,"OBJETO PROXIMO!")
+            layer=sim.setObjectInt32Parameter(obstaculo,10,257)
+        else
+            simUI.setLabelText(uiSensors,2,"")
+            layer=sim.setObjectInt32Parameter(obstaculo,10,256)
+        end
+    elseif (val[1]== 0) then
+        simUI.setLabelText(uiSensors,1,string.format("Sensor1: %.1f mm e Sensor2: %.1f mm",0, val[2]))
+        if val[2]<=160 and val[2]~=0 then
+            simUI.setLabelText(uiSensors,2,"OBJETO PROXIMO!")
+            layer=sim.setObjectInt32Parameter(obstaculo,10,257)
+        else
+            simUI.setLabelText(uiSensors,2,"")
+            layer=sim.setObjectInt32Parameter(obstaculo,10,256)
+        end
+    elseif (val[2]== 0) then
+        simUI.setLabelText(uiSensors,1,string.format("Sensor1: %.1f mm e Sensor2: %.1f mm",val[1], 0))
+        if val[1]<=160 and val[1]~=0 then
+            simUI.setLabelText(uiSensors,2,"OBJETO PROXIMO!")
+            layer=sim.setObjectInt32Parameter(obstaculo,10,257)
+        else
+            simUI.setLabelText(uiSensors,2,"")
+            layer=sim.setObjectInt32Parameter(obstaculo,10,256)
+        end
     end
-    --if ((val[1]/10 <=120 or val[2]/10<=120) and (val[2]~= 0 and val[1]~=0)) then
-    --    simUI.setLabelText(uiSensors,2,string.format("Objeto proximo!"))
-    --end
-    -- Codigo para ler mensagem e comandos do teclado
+    
+    -- Leitura dos sensores virtuais
+    --res,dist=sim.readProximitySensor(ultrasonic)
+    
+    -- Direcoes pelo teclado
     message,auxiliaryData=sim.getSimulatorMessage()
-	while message~=-1 do
-		if (message==sim.message_keypress) then
-			--print(auxiliaryData[1], ...)
-			if (auxiliaryData[1]==string.byte('w')) then
+    while message~=-1 do
+        if (message==sim.message_keypress) then
+            --print(auxiliaryData[1], ...)
+            if (auxiliaryData[1]==string.byte('w')) then
                 tecla = 'w'
                 enviaStringSerial()
-                --print('W')
-                ipos[1] = ipos[1]+0.2;
-                print("Ori Frente X",iori[1],", Y ",iori[2],", Z ",iori[3])
-                sim.setObjectPosition(h,refAbs,{ipos[1],ipos[2],ipos[3]})
             elseif(auxiliaryData[1]==string.byte('s')) then
                 tecla = 's'
                 enviaStringSerial()
-                --print('S')
-                ipos[1] = ipos[1]-0.2;
-                print("Ori Tras X",iori[1],", Y ",iori[2],", Z ",iori[3])
-                sim.setObjectPosition(h,refAbs,{ipos[1],ipos[2],ipos[3]})
             elseif(auxiliaryData[1]==string.byte('a')) then
                 tecla = 'a'
                 enviaStringSerial()
-                --print('A')
-                iori[3] = iori[3]+1.4*(math.pi/180);
-                print("Ori Esquerda X",iori[1],", Y ",iori[2],", Z ",iori[3])
-                sim.setObjectOrientation(h,refAbs,{iori[1],iori[2],iori[3]})
             elseif(auxiliaryData[1]==string.byte('d')) then
                 tecla = 'd'
                 enviaStringSerial()
-                --print('D')
-                iori[3] = iori[3]-1.4*(math.pi/180);
-                print("Ori Direita X",iori[1],", Y ",iori[2],", Z ",iori[3])
-                sim.setObjectOrientation(h,refAbs,{iori[1],iori[2],iori[3]})
             elseif(auxiliaryData[1]==string.byte(' ')) then
                 tecla = 'q'
                 enviaStringSerial()
                 print('PARAR')
-                sim.serialSend(serial," ") -- codigo para parar
-			end
-		end
-		message,auxiliaryData=sim.getSimulatorMessage()
-	end
-
-    -- not wasting time here, we force the switching of the thread
-    sim.switchThread()
+            end
+        end
+        message,auxiliaryData=sim.getSimulatorMessage()
+        andaDirecao()
+    end
+    
+     sim.switchThread() -- resume in next simulation step
 end
 
-tecla = 'p'
+
+-- Put some clean-up code here
+tecla = 'q'
 enviaStringSerial()
+andaDirecao()
 print('FINALIZADO')
 
 sim.serialClose(serial)
